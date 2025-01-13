326bde28233eb18cdd4e9863242b0d460e2c7671
infinit3i <matthewiver@protonmail.com>
2025-01-12 22:21:43 -0500

    push apt also

--- IOC-Converter/converter.py
+++ IOC-Converter/converter.py
@@ -42,7 +42,7 @@ def extract_ips_hashes_domains(data: str) -> Tuple[Set[str], Set[str], Set[str],
 
     return ipv4, ipv6, md5, sha1, sha256, domains
 
-def append_to_csv(filename: str, data: Set[str], data_type: str, malware_family: str) -> None:
+def append_to_csv(filename: str, data: Set[str], data_type: str, malware_family: str, apt_group: str) -> None:
     if not data:  # Skip if the data set is empty
         return
 
@@ -54,10 +54,10 @@ def append_to_csv(filename: str, data: Set[str], data_type: str, malware_family:
     file_path = os.path.join(output_folder, f"{filename}.csv")
 
     # Define the structure of the DataFrame
-    columns = ["Value", "Type", "malware_families", "kill_chains", "severity", "Additional Info"]
+    columns = ["Value", "Type", "malware_families","kill_chains", "severity", "APT_group",  "Additional Info"]
     new_data = pd.DataFrame(
         [
-            [value, data_type, f'"{malware_family.strip()}"', "<kill chain>", "", ""]
+            [value, data_type, f'"{malware_family.strip()}"', "<kill chain>", "", f'"{apt_group.strip()}"', ""]
             for value in data
         ],
         columns=columns,
@@ -72,40 +72,42 @@ def append_to_csv(filename: str, data: Set[str], data_type: str, malware_family:
         combined_data = pd.concat([existing_data, new_data]).drop_duplicates(subset="Value")
         combined_data.to_csv(file_path, index=False)
 
-def generate_suricata_ip_rule(ip: str) -> str:
+        
+def generate_suricata_ip_rule(ip: str, apt_group: str) -> str:
     sid = random.randint(100000000, 9999999999)
     # Rule for IP as the source (sending)
-    sending_rule = f"alert ip {ip} any -> any any (msg:\"Suspicious {base_filename} IP detected Entering Network: {ip} (source)\"; sid:{sid}; rev:1; classtype:trojan-activity;)\n"
+    sending_rule = f'alert ip {ip} any -> any any (msg:"Suspicious {base_filename} IP detected Entering Network: {ip} (source) - APT Group: {apt_group}"; sid:{sid}; rev:1;)\n'
     # Rule for IP as the destination (receiving)
-    receiving_rule = f"alert ip any any -> {ip} any (msg:\"Suspicious {base_filename} IP detected Leaving Network: {ip} (destination)\"; sid:{sid + 1}; rev:1; classtype:trojan-activity;)\n"
+    receiving_rule = f'alert ip any any -> {ip} any (msg:"Suspicious {base_filename} IP detected Leaving Network: {ip} (destination) - APT Group: {apt_group}"; sid:{sid + 1}; rev:1;)\n'
 
     return sending_rule + receiving_rule
 
-def generate_yara_rule(ips, md5_hashes, sha1_hashes, sha256_hashes, domains, creator_name):
+def generate_yara_rule(ips, md5_hashes, sha1_hashes, sha256_hashes, domains, creator_name, apt_group):
     current_date = datetime.datetime.now().strftime("%Y-%m-%d")
-    
-    rule = f"{base_filename}_IOCs {{ \n"
-    rule += "  meta:\n"
-    rule += f"    creator = \"{creator_name}\"\n"
-    rule += f"    date = \"{current_date}\"\n"
-    rule += "    description = \"Suspicious IPs, Hashes, and Domains\"\n"
 
-    rule += "  strings:\n"
+    rule = f'{base_filename}_IOCs {{\n'
+    rule += '  meta:\n'
+    rule += f'    creator = "{creator_name}"\n'
+    rule += f'    date = "{current_date}"\n'
+    rule += f'    description = "Suspicious IPs, Hashes, and Domains"\n'
+    rule += f'    apt_group = "{apt_group}"\n'
+
+    rule += '  strings:\n'
     for ip in ips:
-        rule += f"    $ip_{ip.replace('.', '_')} = \"{ip}\"\n"
+        rule += f'    $ip_{ip.replace(".", "_")} = "{ip}"\n'
     for md5 in md5_hashes:
-        rule += f"    $md5_{md5} = \"{md5}\"\n"
+        rule += f'    $md5_{md5} = "{md5}"\n'
     for sha1 in sha1_hashes:
-        rule += f"    $sha1_{sha1} = \"{sha1}\"\n"
+        rule += f'    $sha1_{sha1} = "{sha1}"\n'
     for sha256 in sha256_hashes:
-        rule += f"    $sha256_{sha256[:8]} = \"{sha256}\"\n"
+        rule += f'    $sha256_{sha256[:8]} = "{sha256}"\n'
     for domain in domains:
-        rule += f"    $domain_{domain.replace('.', '_')} = \"{domain}\"\n"
+        rule += f'    $domain_{domain.replace(".", "_")} = "{domain}"\n'
 
-    rule += "  condition:\n"
-    rule += "    any of them\n"
-    rule += "}\n"
-    
+    rule += '  condition:\n'
+    rule += '    any of them\n'
+    rule += '}\n'
+
     return rule
 
 def generate_rules_from_file(file_path: str, base_filename: str, creator_name: str) -> None:
@@ -113,30 +115,31 @@ def generate_rules_from_file(file_path: str, base_filename: str, creator_name: s
     ip, ipv6, md5, sha1, sha256, domain = extract_ips_hashes_domains(data)
 
     malware_family = input("Is this file associated with any malware family? If yes, please specify (or press Enter to skip): ")
+    apt_group = input("Is this file associated with any APT group? If yes, please specify (or press Enter to skip): ")
 
-    append_to_csv("md5", md5, "md5", malware_family)
-    append_to_csv("sha1", sha1, "sha1", malware_family)
-    append_to_csv("sha256", sha256, "sha256", malware_family)
-    append_to_csv("ip", ip, "IPv4", malware_family)
-    append_to_csv("ipv6", ipv6, "IPv6", malware_family)
-    append_to_csv("domains", domain, "domain", malware_family)
+    append_to_csv("md5", md5, "md5", malware_family, apt_group)
+    append_to_csv("sha1", sha1, "sha1", malware_family, apt_group)
+    append_to_csv("sha256", sha256, "sha256", malware_family, apt_group)
+    append_to_csv("ip", ip, "IPv4", malware_family, apt_group)
+    append_to_csv("ipv6", ipv6, "IPv6", malware_family, apt_group)
+    append_to_csv("domains", domain, "domain", malware_family, apt_group)
 
-    suricata_rules: List[str] = [generate_suricata_ip_rule(ip) for ip in ip]
-    
+    suricata_rules: List[str] = [generate_suricata_ip_rule(ip, apt_group) for ip in ip]
+
     current_date = datetime.datetime.now().strftime("%Y%m%d")
     suricata_filename = f"{base_filename}-suricata-{current_date}.txt"
     yara_filename = f"{base_filename}-yara-{current_date}.yar"
-    
+
     output_folder = "CREATED_IOCs"
     os.makedirs(output_folder, exist_ok=True)
-    
+
     suricata_file_path = os.path.join(output_folder, suricata_filename)
     yara_file_path = os.path.join(output_folder, yara_filename)
 
     with open(suricata_file_path, "w") as suricata_file:
         suricata_file.writelines(suricata_rules)
 
-    yara_rule = generate_yara_rule(ip, md5, sha1, sha256, domain, creator_name)
+    yara_rule = generate_yara_rule(ip, md5, sha1, sha256, domain, creator_name, apt_group)
     with open(yara_file_path, "w") as yara_file:
         yara_file.write(yara_rule)
 
